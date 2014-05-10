6b7db3e915ded9f033edf8aeb43a5882d4108b17
Cristian Valles <valles.cristian1992@gmail.com>
2014-05-10 14:23:06 -0400

    inicio con devise

--- db/migrate/20140510173653_add_devise_to_players.rb
+++ db/migrate/20140510173653_add_devise_to_players.rb
@@ -35,7 +35,7 @@ class AddDeviseToPlayers < ActiveRecord::Migration
       # t.timestamps
     end
 
-    add_index :players, :email,                unique: true
+    add_index :players, :email
     add_index :players, :reset_password_token, unique: true
     # add_index :players, :confirmation_token,   unique: true
     # add_index :players, :unlock_token,         unique: true
