8f7abac3076835773d7b8e825ccad107b3ec7be5
Franz Wompner <fwompner@gmail.com>
2014-06-02 16:22:59 -0400

    Fixed callback

--- app/controllers/players/omniauth_callbacks_controller.rb
+++ app/controllers/players/omniauth_callbacks_controller.rb
@@ -2,7 +2,7 @@ class Players::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 	def facebook
 		@player = Player.find_for_facebook_oauth(request.env["omniauth.auth"])
 
-    if @user.persisted?
+    if @player.persisted?
       sign_in_and_redirect @player, :event => :authentication
       set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
     else
--- app/models/player.rb
+++ app/models/player.rb
@@ -5,4 +5,14 @@ class Player < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable,
 		 :omniauthable, :omniauth_providers => [:facebook]
   has_many :friends, :dependent => :destroy
+
+		def self.find_for_facebook_oauth(auth)
+			where(auth.slice(:provider, :uid)).first_or_create do |player|
+				player.provider = auth.provider
+				player.uid = auth.uid
+				player.email = auth.info.email
+				player.first_name = auth.info.first_name
+				player.last_name = auth.info.last_name
+			end
+		end
 end
