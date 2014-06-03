ebc2d7d81215f4d7f39be5693ef1d1a564ce9d9f
Franz Wompner <fwompner@gmail.com>
2014-06-03 08:45:26 -0400

    Transform tabs into whitespace

--- app/controllers/players/omniauth_callbacks_controller.rb
+++ app/controllers/players/omniauth_callbacks_controller.rb
@@ -1,15 +1,15 @@
 class Players::OmniauthCallbacksController < Devise::OmniauthCallbacksController
-	def facebook
-		@player = Player.find_for_facebook_oauth(request.env["omniauth.auth"])
+  def facebook
+    @player = Player.find_for_facebook_oauth(request.env["omniauth.auth"])
 
-		if @player.persisted?
-		  sign_in_and_redirect @player, :event => :authentication
-		  set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
-		else
-		  session["devise.facebook_data"] = request.env["omniauth.auth"]
-			@player.save
+    if @player.persisted?
+      sign_in_and_redirect @player, :event => :authentication
+      set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
+    else
+      session["devise.facebook_data"] = request.env["omniauth.auth"]
+      @player.save
 
-		  redirect_to root_path
-		end
-	end
+      redirect_to root_path
+    end
+  end
 end
