14e1c79d9aba96e06eddd90c7477d08aa3915448
stilexmagic <valles.cristian1992@gmail.com>
2014-06-17 02:41:35 -0400

    borrando la coma en la migración de player_uid

--- db/migrate/20140617025850_change_uid_to_long_type.rb
+++ db/migrate/20140617025850_change_uid_to_long_type.rb
@@ -1,5 +1,5 @@
 class ChangeUidToLongType < ActiveRecord::Migration
   def change
-    change_column, :players, :uid, :integer, :limit => 8
+    change_column :players, :uid, :integer, :limit => 8
   end
 end
