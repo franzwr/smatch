28aa292d26aca47a901bdc6090b48e449f526249
Franz Wompner <fwompner@gmail.com>
2014-06-18 10:36:04 -0400

    Fixed friendship generation at omniauth callback

--- app/controllers/players/omniauth_callbacks_controller.rb
+++ app/controllers/players/omniauth_callbacks_controller.rb
@@ -10,22 +10,22 @@ class Players::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 	end
 
     # add friends
-	if Player.count > 1
-	  graph = FbGraph::User.me(oauth.credentials.token)
-
-      for f in graph.friends
-        u = Player.find_by_uid(f.identifier)
-		if u != nil
-		  # if no friendship is found... create it
-          if Friendship.where("player_id = :p1 and friend_id = :p2 or
-                               player_id = :p2 and friend_id = :p1",
-                               p1: @player.id, p2: u.id).empty?
-		    Friendship.new(user_id: @player.id, friend_id: u.id, confirmed: 1).save
-		    Friendship.new(friend_id: @player.id, user_id: u.id, confirmed: 1).save
-          end
-		end
-      end
-    end
+ 	if Player.count > 1
+ 	  graph = FbGraph::User.me(oauth.credentials.token)
+ 
+       for f in graph.friends
+         u = Player.find_by_uid(f.identifier)
+ 		if u != nil
+ 		  # if no friendship is found... create it
+           if Friendship.where("player_id = :p1 and friend_id = :p2 or
+                                player_id = :p2 and friend_id = :p1",
+                                p1: @player.id, p2: u.id).empty?
+ 		    Friendship.new(player_id: @player.id, friend_id: u.id, confirmed: 1).save
+ 		    Friendship.new(friend_id: @player.id, player_id: u.id, confirmed: 1).save
+           end
+ 		end
+       end
+     end
     
 	sign_out(@player) if player_signed_in?
 	redirect_to root_path
